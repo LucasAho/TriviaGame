5ad6a7d46aea48d5323d31b50fe2f5099d675586
Lucas Aho <aholucas11@gmail.com>
2019-11-02 15:30:38 -0600

    added images to each question

--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -1,47 +1,50 @@
-//This script seeks to utilize card like quiz objects written for each question, rather than randomly generated and possibly repeating questions
-
-
 //Objects for each question
 var cardObj = [    
     cardOne = {
         quest: "Which big cat has the longest tail?",
         ansSet: ["Snow Leopard","Tiger","Cheetah","Lion"],
         rightAns: "0",
-        strAns: "Snow Leopard"
+        strAns: "Snow Leopard",
+        pic: "leopard"
+
     }, 
     cardTwo = {
         quest: "What does a guinea pig do when it gets excited?",
         ansSet: ["Binky","Peanut","Back-flip","Roll over"],
         rightAns: "1",
-        strAns: "Peanut"
+        strAns: "Peanut",
+        pic: "pig"
     },   
     cardThree = {
         quest: "What is a baby llama called?",
         ansSet: ["Kid","Cub","Squirt","Cria"],
         rightAns: "3",
-        strAns: "Cria"
+        strAns: "Cria",
+        pic: "llama"
     },
     cardFour = {
         quest: "Where are the majority of an octopod's neurons?",
         ansSet: ["The brain", "The arms", "The stomach", "The beak"],
         rightAns: "1",
-        strAns: "The arms"
+        strAns: "The arms",
+        pic: "octo"
     },
     cardFive = {
         quest: "What is the fastest swimmer in the ocean?",
         ansSet: ["Great-White Shark", "Bottlenose Dolphin", "Sailfish", "Mako Shark"],
         rightAns: "2",
-        strAns: "Sailfish"
+        strAns: "Sailfish",
+        pic: "sail"
     },
     cardSix =  {
         quest: "How long do crows mate?",
         ansSet: ["For a season", "For life", "Once", "Till their baby leaves"],
         rightAns: "1",
         strAns: "For life",
+        pic: "crow"
 
     },
 ];
-
 
 
 //Scorekeeping variables for player
@@ -54,6 +57,7 @@ var currQuest = "";
 var currAns = [];
 var currRig = "";
 var currStr = "";
+var currPic = "";
 
 var timeLeft = 0;
 var intervalId;
@@ -97,7 +101,7 @@ game = {
         }
     },
     endCard: () => {
-        
+        $("#phImg").empty();
         $("#questionBox").html("All done, here's how you did!");
         $("#ansBox").html("Correct Answers: " + corrAns + "<br> Incorrect Answers: " + incoAns + "<br> Unanswered: " + noAns);
         $("#ansBox").append("<div class= 'col-12'><button type='button' id='startbtn'>Start Over?</button></div>");
@@ -121,7 +125,7 @@ game = {
         } 
         $("#questionBox").html(status);
         $("#ansBox").html("The correct answer is: " + currStr);
-        
+        $("#phImg").html($("<img src='assets/images/" + currPic + ".jpg'>"));
     },
 
     ansCheck: (z) => {  
@@ -144,10 +148,12 @@ game = {
         var tempObj = cardObj;
         game.runTimer(30);    
         $("#ansBox").empty();
+        $("#phImg").empty();
         currQuest = tempObj[num].quest;
         currAns = tempObj[num].ansSet;
         currRig = tempObj[num].rightAns;   
-        currStr = tempObj[num].strAns;       
+        currStr = tempObj[num].strAns; 
+        currPic = tempObj[num].pic;     
         $("#timeBox").text(timeLeft);
         $("#questionBox").html(currQuest);
         $("#ansBox").append("<button class= 'col-12' id='0'>"+currAns[0]+"</button>");
